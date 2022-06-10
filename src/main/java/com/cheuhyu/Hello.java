4ae07fb5486e2aea9b47fb48fd90812c54ae03fb
chuehyu <a230168988@gmail.com>
2022-06-10 15:07:44 +0800

    Person

---
+++ src/main/java/com/cheuhyu/Hello.java
@@ -0,0 +1,7 @@
+package com.cheuhyu;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
---
+++ src/main/java/com/cheuhyu/Person.java
@@ -0,0 +1,7 @@
+package com.cheuhyu;
+
+public class Person {
+    public void greeting() {
+        System.out.println("Hello");
+    }
+}
