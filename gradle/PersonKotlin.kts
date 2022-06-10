054f3e72531c50bdf8c660dade5ecf97a113b036
chuehyu <a230168988@gmail.com>
2022-06-10 16:26:49 +0800

    Hello world with Kotlin

---
+++ gradle/PersonKotlin.kts
@@ -0,0 +1,13 @@
+package com.chuehyu
+
+class PersonKotlin {
+    fun greeting() {
+        println("hello world")
+    }
+}
+
+//Top-level
+fun main() {
+    val p = PersonKotlin()
+    p.greeting()
+}
